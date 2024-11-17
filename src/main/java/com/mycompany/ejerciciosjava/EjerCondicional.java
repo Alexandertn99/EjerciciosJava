e11e90cd8c1983a8071402c29216020a31cde626
Alexandertn99 <tantahuillcanavarroalexander@gmail.com>
2024-11-17 10:22:22 -0500

    Mi primer Commit con Netbeans

---
+++ src/main/java/com/mycompany/ejerciciosjava/EjerCondicional.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.ejerciciosjava;
+
+/**
+ *
+ * @author ALEXANDER
+ */
+public class EjerCondicional {
+    //int,double,string,char,boolean    variables primitivas
+    
+ 
+    
+  
+   public static void main(String[] args) {
+       
+    int edad=19;
+    boolean esMayor=false;
+        System.out.println("Mi edad es: "+edad);
+        
+        if(edad>17){
+            System.out.println("Soy mayor de edad con: "+edad);
+        }else{
+             System.out.println("Soy menor de edad con: "+edad);
+        }
+        
+    } 
+    
+    
+    
+}
